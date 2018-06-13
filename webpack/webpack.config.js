24198385c22b91716ba2925e376df04531d1b04a
jeroenrodenburg <jeroen@rodenburg.io>
2018-06-13 20:47:51 +0200

    commit

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,7 @@ const loaders = require('./loaders');
 const plugins = require('./plugins');
 
 module.exports = {
-  entry: ["./src/js/app.js"],
+  entry: ["./src/index.js"],
   module: {
     rules: [
       loaders.CSSLoader,
@@ -19,4 +19,10 @@ module.exports = {
     path: path.resolve(__dirname, "../dist"),
     filename: "[name].bundle.js"
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    stats: 'errors-only'
+  }
 };
