415cbffcfe2c617d7d39f6f8ab9e1ff076fc3a10
jeroenrodenburg <jeroen@rodenburg.io>
2018-06-21 18:55:03 +0200

    switch function improvements, gradient and mobile

--- assets/scripts/modules/SwitchButton.js
+++ assets/scripts/modules/SwitchButton.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class SwitchButton {
   constructor() {
     this.sliderButton = $(".slider");
+    this.headerBackground = $(".site-header__overlay");
     this.workContent = $(".page-section__work");
     this.aboutContent = $(".page-section__about");
     this.events();
@@ -13,6 +14,7 @@ class SwitchButton {
   }
 
   toggleSwitch() {
+    this.headerBackground.toggleClass("site-header__overlay--is-checked")
     this.workContent.toggleClass("page-section__work--is-visible")
     this.aboutContent.toggleClass("page-section__about--is-hidden")
   }
